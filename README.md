717ecc73167090b3688b81933123b4fdb67aa6c6
joyful-young <isglory22@gmail.com>
2023-04-27 23:10:41 +0900

    ch1 정리 완료

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Vue_prac
+
+## 고양이도 할 수 있는 Vue.js 읽고 따라해보기
+
+[지원 페이지](https://rintiantta.github.io/jpub-vue/)
+
+[원서 지원 페이지](https://github.com/mio3io/cr-vue)
--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 const app = new Vue({
+  // 옵션
   el: '#app',
+  data: {
+    message: '',
+    list: ['사과', '바나나', '딸기'],
+    show: true,
+  },
+  methods: {
+    handleClick: function(event) {
+      alert(event.target)
+    },
+  },
 }) 
