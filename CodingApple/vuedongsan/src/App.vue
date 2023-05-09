232858e3301cfc05d08f3af0693a57c7cd206eaf
joyful-young <isglory22@gmail.com>
2023-05-09 23:32:26 +0900

    230509 코딩애플 모달창 만드는 법

---
+++ CodingApple/vuedongsan/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+
+    <!-- 모달창 디자인 -->
+    <div class="black-bg" v-if="modal">
+      <div class="white-bg">
+        <h4>상세페이지</h4>
+        <p>내용</p>
+        <button @click="modal = false">close</button>
+      </div>
+    </div>
+
+    <h1>부동산앱</h1>
+
+    <div>
+      <h4 @click="modal = true">XX 원룸</h4>
+      <p>{{ price1 }} 만원</p>
+    </div>
+    <div>
+      <h4>XX 원룸</h4>
+      <p>{{ price2 }} 만원</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      price1: 60,
+      price2: 70,
+
+      // 모달창 상태
+      modal: false,
+    }
+  },
+  components: {
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 [지원 페이지](https://rintiantta.github.io/jpub-vue/)
 
 [원서 지원 페이지](https://github.com/mio3io/cr-vue)
+
+## 코딩애플 Vue 강의
+
+1. 부동산 웹앱 만들기
